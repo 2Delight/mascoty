9caff5b2f902405098929e7e38e374f798cc9ab0
Mmmr7 <ol-sidorenkov@mail.ru>
2023-01-18 12:21:24 +0300

    better tests arch

--- builder/src/config/tests.rs
+++ builder/src/config/tests.rs
@@ -1,27 +1,24 @@
-#[cfg(test)]
-mod tests {
-    use crate::config::import_config;
-    
-    #[test]
-    fn check_config_right_path() {
-        import_config("src/config/config.yaml").unwrap();
-    }
-    
-    #[test]
-    #[should_panic]
-    fn check_config_wrong_path() {
-        import_config("it is not even a path lol").unwrap();
-    }
-    
-    #[test]
-    fn check_url() {
-        let conf = import_config("src/config/config.yaml").unwrap();
-        assert!(conf.service.url.len() > 0);
-    }
-    
-    #[test]
-    fn check_port() {
-        let conf = import_config("src/config/config.yaml").unwrap();
-        assert!(conf.service.port > 0);
-    }
+use crate::config::import_config;
+
+#[test]
+fn check_config_right_path() {
+    import_config("src/config/config.yaml").unwrap();
+}
+
+#[test]
+#[should_panic]
+fn check_config_wrong_path() {
+    import_config("it is not even a path lol").unwrap();
+}
+
+#[test]
+fn check_url() {
+    let conf = import_config("src/config/config.yaml").unwrap();
+    assert!(conf.service.url.len() > 0);
+}
+
+#[test]
+fn check_port() {
+    let conf = import_config("src/config/config.yaml").unwrap();
+    assert!(conf.service.port > 0);
 }
